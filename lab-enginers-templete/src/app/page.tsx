4aaa4d32c8ea15fd80d8656feca12f376381f687
Shahidul Shohid <shahidulislamshohid7@gmail.com>
2025-08-03 09:30:07 +0600

    project description two is done

--- lab-enginers-templete/src/app/page.tsx
+++ lab-enginers-templete/src/app/page.tsx
@@ -1,6 +1,7 @@
 import ProjectDescriptionEight from "./(alTemplete)/file-one/project-description-eight/page"
 import ProjectDescriptionNine from "./(alTemplete)/file-one/project-description-nine/page"
 import ProjectDescriptionOne from "./(alTemplete)/file-one/project-description-one/page"
+import ProjectDescriptionTwo from "./(alTemplete)/file-one/project-sescription-two/page"
 
 
 
@@ -8,8 +9,9 @@ function Home() {
   return (
     <div className='p-8'>
       {/* <ProjectDescriptionOne/> */}
-      <ProjectDescriptionEight/>
+      {/* <ProjectDescriptionEight/> */}
       {/* <ProjectDescriptionNine/> */}
+      <ProjectDescriptionTwo/>
     </div>
   )
 }
