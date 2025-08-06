e799df693d0e4abbbf74c893c086d7913f6f75fa
Md Israkul Islam <israkulislam@gmail.com>
2025-08-06 09:57:41 +0600

    added

--- lab-enginers-templete/src/app/page.tsx
+++ lab-enginers-templete/src/app/page.tsx
@@ -5,9 +5,32 @@ import ProjectDescriptionNine from "./(alTemplete)/file-one/project-description-
 import ProjectDescriptionOne from "./(alTemplete)/file-one/project-description-one/page"
 import ProjectDescriptionTen from "./(alTemplete)/file-one/project-description-ten/page"
 import ProjectDescriptionTwo from "./(alTemplete)/file-one/project-description-two/page"
+import NineFileDesignEight from "./(alTemplete)/NineFileDesignEight/page"
 import NineFileDesignEleven from "./(alTemplete)/NineFileDesignEleven/page"
 import NineFileDesignFive from "./(alTemplete)/NineFileDesignFive/page"
+import NineFileDesignFour from "./(alTemplete)/NineFileDesignFour/page"
+import NineFileDesignNine from "./(alTemplete)/NineFileDesignNine/page"
+import NineFileDesignOne from "./(alTemplete)/NineFileDesignOne/page"
+import NineFileDesinSeven from "./(alTemplete)/NineFileDesignSeven/page"
+import NineFileDesignSix from "./(alTemplete)/NineFileDesignSix/page"
+
 import NineFileDesignTen from "./(alTemplete)/NineFileDesignTen/page"
+import NineFileDesignThirteen from "./(alTemplete)/NineFileDesignThirteen/page"
+import NineFileDesignThree from "./(alTemplete)/NineFileDesignThree/page"
+import NineFileDesignTwelve from "./(alTemplete)/NineFileDesignTwelve/page"
+import NineFileDesignTwo from "./(alTemplete)/NineFileDesignTwo/page"
+import TenFileDesignEight from "./(alTemplete)/TenFileDesignEight/page"
+import TenFileDesignEleven from "./(alTemplete)/TenFileDesignEleven/page"
+
+import TenFileDesignFour from "./(alTemplete)/TenFileDesignFour/page"
+import TenFileDesignNine from "./(alTemplete)/TenFileDesignNine/page"
+import TenFileDesignSeven from "./(alTemplete)/TenFileDesignSeven/page"
+import TenFileDesignSix from "./(alTemplete)/TenFileDesignSix/page"
+import TenFileDesignTen from "./(alTemplete)/TenFileDesignTen/page"
+import TenFileDesignThirteen from "./(alTemplete)/TenFileDesignThirteen/page"
+import TenFileDesignThree from "./(alTemplete)/TenFileDesignThree/page"
+import TenFileDesignTwelve from "./(alTemplete)/TenFileDesignTwelve/page"
+import TenFileDesignFive from "./TenFileDesignFive/page"
 
 
 
@@ -21,9 +44,33 @@ function Home() {
       {/* <ProjectDescriptionTen/> */}
       {/* <ProjectDescriptionEleven/> */}
       {/* <DesignFive/> */}
+      {/* <NineFileDesignOne></NineFileDesignOne> */}
+      {/* <NineFileDesignTwo></NineFileDesignTwo> */}
+      {/* <NineFileDesignThree></NineFileDesignThree> */}
+      {/* <NineFileDesignFour></NineFileDesignFour> */}
       {/* <NineFileDesignFive/> */}
-      <NineFileDesignTen></NineFileDesignTen>
+      {/* <NineFileDesignSix></NineFileDesignSix> */}
+      {/* <NineFileDesinSeven></NineFileDesinSeven> */}
+      {/* <NineFileDesignEight></NineFileDesignEight> */}
+      {/* <NineFileDesignNine></NineFileDesignNine> */}
+      {/* <NineFileDesignTen></NineFileDesignTen> */}
       {/* <NineFileDesignEleven></NineFileDesignEleven> */}
+     {/* <NineFileDesignTwelve></NineFileDesignTwelve> */}
+      <NineFileDesignThirteen></NineFileDesignThirteen>
+      {/* <TenFileDesignThree></TenFileDesignThree> */}
+      {/* <TenFileDesignFour></TenFileDesignFour> */}
+      {/* <TenFileDesignFive></TenFileDesignFive> */}
+      {/* <TenFileDesignSix></TenFileDesignSix> */}
+      {/* <TenFileDesignSeven></TenFileDesignSeven> */}
+      {/* <TenFileDesignEight></TenFileDesignEight> */}
+      {/* <TenFileDesignNine></TenFileDesignNine> */}
+      {/* <TenFileDesignTen></TenFileDesignTen> */}
+      {/* <TenFileDesignEleven></TenFileDesignEleven> */}
+      {/* <TenFileDesignTwelve></TenFileDesignTwelve> */}
+      {/* <TenFileDesignThirteen></TenFileDesignThirteen> */}
+      
+      
+      
     </div>
   )
 }
